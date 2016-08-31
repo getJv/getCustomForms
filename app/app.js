7e56ea5eb9c99b1b78ea7958e18afed28beb04ff
Jhonatan Morais <jhonatanvinicius@gmail.com>
2016-08-30 23:43:42 -0300

    normal

--- app/app.js
+++ app/app.js
@@ -48,22 +48,64 @@
 
 	}]);
 	
-	app.controller('DtableController', ['DTOptionsBuilder','DTColumnBuilder',function(DTOptionsBuilder, DTColumnBuilder){
+	app.controller('DtableController', ['$compile', '$scope','DTOptionsBuilder','DTColumnBuilder',function($compile, $scope,DTOptionsBuilder, DTColumnBuilder){
+		
+		var titleHtml = '<input type="checkbox" ng-model="showCase.selectAll" ng-click="showCase.toggleAll(showCase.selectAll, showCase.selected)">';
 		var vm = this;
+		 vm.selected = {};
+		vm.selectAll = false;
+		vm.toggleAll = toggleAll;
+		vm.toggleOne = toggleOne;
+		
 		vm.dtOptions = DTOptionsBuilder.fromSource('server/server.php?function=getSurveys')
+		
+		.withOption('createdRow', function(row, data, dataIndex) {
+            // Recompiling so we can bind Angular directive to the DT
+            $compile(angular.element(row).contents())($scope);
+        })
+        .withOption('headerCallback', function(header) {
+            if (!vm.headerCompiled) {
+                // Use this headerCompiled field to only compile header once
+                vm.headerCompiled = true;
+                $compile(angular.element(header).contents())($scope);
+            }
+        })
 		.withPaginationType('full_numbers');
 			
 		vm.dtColumns = [
 			
-			//DTColumnBuilder.newColumn('Actions','foo'),
-			DTColumnBuilder.newColumn('id','Action2'),
-			DTColumnBuilder.actionColumn('terter'),
-			DTColumnBuilder.newColumn('name').withTitle('name')
+			DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
+            .renderWith(function(data, type, full, meta) {
+                vm.selected[full.id] = false;
+                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']" ng-click="showCase.toggleOne(showCase.selected)">';
+            }),
+			DTColumnBuilder.newColumn('id','ID'),
+			DTColumnBuilder.newColumn('name').withTitle('name'),
+			DTColumnBuilder.newColumn(null).withTitle('name')
 			.renderWith(function(data, type, full) {
             return full.name + " <input type='button'  value='"+full.id+"'/>";
         })
 			
 		];
+		
+		function toggleAll (selectAll, selectedItems) {
+			for (var id in selectedItems) {
+				if (selectedItems.hasOwnProperty(id)) {
+					selectedItems[id] = selectAll;
+				}
+			}
+		}
+		function toggleOne (selectedItems) {
+			for (var id in selectedItems) {
+				if (selectedItems.hasOwnProperty(id)) {
+					if(!selectedItems[id]) {
+						vm.selectAll = false;
+						return;
+					}
+				}
+			}
+			vm.selectAll = true;
+		}
 	
 		
 		
