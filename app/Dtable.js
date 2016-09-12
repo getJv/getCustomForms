09cb5b056cc8c4c70f7f80ce608e43642d196043
Jhonatan Morais <jhonatanvinicius@gmail.com>
2016-09-12 07:39:33 -0300

    atu

--- app/Dtable.js
+++ app/Dtable.js
@@ -1,20 +1,15 @@
 (function(){
 	
-	angular.module('showcase.withAjax', ['datatables']).controller('WithAjaxCtrl', WithAjaxCtrl);
+	angular.module('angularWay', ['datatables', 'ngResource']).controller('AngularWayCtrl', AngularWayCtrl);
 
-	function WithAjaxCtrl(DTOptionsBuilder, DTColumnBuilder) {
-		var vm = this;
+	function AngularWayCtrl($resource) {
+		 var vm = this;
+		$resource('app/test-data.json').query().$promise.then(function(persons) {
+			vm.persons = persons;
+		});
 		
 				
-		//console.log('test-data.json');
-		vm.dtOptions = DTOptionsBuilder.fromSource('app/test-data.json')
-			.withPaginationType('full_numbers');
-		vm.dtColumns = [
-			
-			//DTColumnBuilder.newColumn('name').withTitle('name'),
-			DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-			DTColumnBuilder.newColumn('lastName').withTitle('Last name')//.notVisible()
-		];
+		
 	}
 	
 	
--- app/app.js
+++ app/app.js
@@ -37,82 +37,33 @@
 		survey.surveys = survey.getSurveys();	
 		 
 		
-		
-		 
-		
-	
-		
-		
-
-		
+			
 
 	}]);
 	
-	app.controller('DtableController', ['$compile', '$scope','DTOptionsBuilder','DTColumnBuilder',function($compile, $scope,DTOptionsBuilder, DTColumnBuilder){
+	app.controller('AngularWayCtrl', ['$http',function($http){
 		
-		var titleHtml = '<input type="checkbox" ng-model="showCase.selectAll" ng-click="showCase.toggleAll(showCase.selectAll, showCase.selected)">';
 		var vm = this;
-		 vm.selected = {};
-		vm.selectAll = false;
-		vm.toggleAll = toggleAll;
-		vm.toggleOne = toggleOne;
+		vm.nome = 'jhonatan';
 		
-		vm.dtOptions = DTOptionsBuilder.fromSource('server/server.php?function=getSurveys')
+		vm.getSurveys = function(){
+			
+			$http.get('server/server.php?function=getSurveys')
+			//$http.get('app/test-data.json')
+			.success(function(res){
+					 //console.log(res);
+                       vm.surveys =  res;
+					})
+			.error(function(error){
+                        console.log(error);
+					});
+			
 		
-		.withOption('createdRow', function(row, data, dataIndex) {
-            // Recompiling so we can bind Angular directive to the DT
-            $compile(angular.element(row).contents())($scope);
-        })
-        .withOption('headerCallback', function(header) {
-            if (!vm.headerCompiled) {
-                // Use this headerCompiled field to only compile header once
-                vm.headerCompiled = true;
-                $compile(angular.element(header).contents())($scope);
-            }
-        })
-		.withPaginationType('full_numbers');
 			
-		vm.dtColumns = [
-			
-			DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
-            .renderWith(function(data, type, full, meta) {
-                vm.selected[full.id] = false;
-                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']" ng-click="showCase.toggleOne(showCase.selected)">';
-            }),
-			DTColumnBuilder.newColumn('id','ID'),
-			DTColumnBuilder.newColumn('name').withTitle('name'),
-			DTColumnBuilder.newColumn(null).withTitle('name')
-			.renderWith(function(data, type, full) {
-            return full.name + " <input type='button'  value='"+full.id+"'/>";
-        })
-			
-		];
-		
-		function toggleAll (selectAll, selectedItems) {
-			for (var id in selectedItems) {
-				if (selectedItems.hasOwnProperty(id)) {
-					selectedItems[id] = selectAll;
-				}
-			}
-		}
-		function toggleOne (selectedItems) {
-			for (var id in selectedItems) {
-				if (selectedItems.hasOwnProperty(id)) {
-					if(!selectedItems[id]) {
-						vm.selectAll = false;
-						return;
-					}
-				}
-			}
-			vm.selectAll = true;
-		}
+		};
+		vm.surveys = vm.getSurveys();	
 	
-		
-		
-		
+	
 	}]);
-
-	
-
 })();
 
